1d92293705587519dbc3aa72283039ca16bc7432
Christopher Whitney <chrisswhitneyy@gmail.com>
2024-10-21 21:02:52 -0700

    Updating CSRF trusted origins

--- fhma/settings.py
+++ fhma/settings.py
@@ -36,7 +36,7 @@ ALLOWED_HOSTS = ["*"]
 
 # FORM SUBMISSION
 # Comment out the following line and place your railway URL, and your production URL in the array.
-CSRF_TRUSTED_ORIGINS = ["fhma-portal-production.up.railway.app"]
+CSRF_TRUSTED_ORIGINS = ["https://fhma-portal-production.up.railway.app", "http://127.0.0.1"]
 
 # Application definition
 
