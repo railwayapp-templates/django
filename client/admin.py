a163e3b32d81f45834dbdf654c9e6acdb7694431
Christopher Whitney <chrisswhitneyy@gmail.com>
2024-11-30 10:35:01 -0700

    Updated supplies client inline display to show supplies

--- client/admin.py
+++ client/admin.py
@@ -5,20 +5,26 @@ from unfold.admin import ModelAdmin, TabularInline
 
 from .models import Client, AreaServiced
 from .forms import ClientForm
-from supplies.models import SuppliesOrder
+from supplies.models import SuppliesOrder, OrderItem
 from equipment.models import Order
 
 class SuppliesInline(TabularInline):
     model = SuppliesOrder
-    fields = ["delivery_date"]
-    readonly_fields = ["delivery_date"]
-    ordering_field = "delivery_date"
+    fields = ["delivery_date", "order_supplies"]
+    readonly_fields = ["delivery_date", "order_supplies"]
+    ordering_field = ["delivery_date", "order_supplies"]
     max_num = 0
     show_change_link = True
     tab = True
     can_delete = False
     verbose_name = "Inconnient Supplies Order"
     verbose_name_plural = "Inconnient Supplies Orders"
+
+    def order_supplies(self, obj):
+        # Display the supplies order items associated with the order
+        items = OrderItem.objects.filter(order=obj)
+        # Display supplies name and quantity 
+        return ", ".join([f"{item.supplies.name} ({item.quantity})" for item in items])
 
 class EquipmentInline(TabularInline):
     model = Order
